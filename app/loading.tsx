bec764ee0d5182e57c398b74126886ff3b5f3b83
Al Aqmar <alaqmarak0810@gmail.com>
2025-04-16 15:06:04 +0530

    loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,18 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="w-[200px] fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0">
-      <div className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
-        <div className="flex flex-col items-center justify-center gap-4 py-8">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-          <div className="text-center">
-            <h3 className="text-lg font-semibold">Loading</h3>
-            <p className="text-sm text-muted-foreground">
-              We&apos;re preparing your content...
-            </p>
-          </div>
-        </div>
+    <div
+    className="flex flex-col w-full h-full items-center justify-center space-y-4"
+    >
+      <Card>
+    <CardHeader className="flex flex-row items-center justify-between pb-2">
+      <CardTitle className="text-sm font-medium text-muted-foreground">
+        Connecting to the server...
+      </CardTitle>
+      <Loader2/>
+    </CardHeader>
+    <CardContent>
+      <div className="text-2xl font-bold">
+        Please wait while we fetch the data
       </div>
+      <p className="text-xs text-muted-foreground mt-1">Powered by https://alaqmar.dev</p>
+    </CardContent>
+  </Card>
     </div>
   )
 }
