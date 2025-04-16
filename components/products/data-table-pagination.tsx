a7d4c699da08771f1949b20bdf65870e57c1fdd5
Al Aqmar <alaqmarak0810@gmail.com>
2025-04-16 15:28:21 +0530

    tabular changes

--- components/products/data-table-pagination.tsx
+++ components/products/data-table-pagination.tsx
@@ -42,7 +42,7 @@ export function DataTablePagination<TData>({
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex items-center space-x-6 lg:space-x-8">
+      <div className="flex flex-col items-start gap-y-4">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
